a4e94e43c66afe7f2fa9f3e864f0ec5971132164
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-04-16 17:43:26 -0700

    Add forgotten author note
    Change-Id: I1307e5c12ff8c854aba83fdf10980ce56d0c124d

--- adhoc/adhoc-osx.mm
+++ adhoc/adhoc-osx.mm
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+ *         Ilya Moiseenko <iliamo@ucla.edu>
  *         Zhenkai Zhu <zhenkai@cs.ucla.edu>
  */
 
